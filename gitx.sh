9b7a23c8cd40ea0f960c2fa838eb707c62659ad8
ijulhaxor <ijul@localhost.localdomain>
2021-07-17 21:15:14 +0700

    first commit

---
+++ gitx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+read -p "Repository name : " repo
+read -p "Add file?       : " addfile
+echo "# $repo" >> README.md
+git init
+git add $addfile
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ijulhaxor/$repo.git
+git push -u origin main
